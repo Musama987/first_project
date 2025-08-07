48081dd02efcb77fe1d63165cab47724671952a6
M Usama Ramzan <usamaramzan215@gmail.com>
2025-08-07 13:45:47 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text("Aja bhai"),
+        ),
+        body: Center(
+          child: Text("Hi How are You!"),
+        ),
+      ),
+    );
+  }
+}
